526ce81f17518d5dae205432a6197e11a5065feb
Scott O'Malley <somalley@guidewire.com>
2017-04-21 13:41:05 +0100

    Added Inversion function

--- terreract.py
+++ terreract.py
@@ -1,15 +1,31 @@
 from PIL import Image, ImageEnhance, ImageFilter, ImageChops
 import pytesseract
+import numpy as np
 
 #Print the file
 
-image = Image.open('3.png')
-image = ImageEnhance.Color(image).enhance(0.0)
-image = ImageChops.invert(image)
-image = ImageEnhance.Contrast(image).enhance(1000).show()
+# image = Image.open('3.png')
+# image = ImageEnhance.Color(image).enhance(0.0)
+# image = ImageChops.invert(image)
+# image = ImageEnhance.Contrast(image).enhance(1000).show()
 
 
+col = Image.open("3.png")
+gray = col.convert('L')
+
+# Let numpy do the heavy lifting for converting pixels to pure black or white
+bw = np.asarray(gray).copy()
+
+# Pixel range is 0...255, 256/2 = 128
+bw[bw < 200] = 0    # Black
+bw[bw >= 200] = 255 # White
+
+# Now we put it back in Pillow/PIL land
+imfile = Image.fromarray(bw)
+imfile = ImageChops.invert(imfile)
+
+# imfile.crop((left, top, right, bottom))
 # contrast = ImageEnhance.Contrast(image).enhance(2)
 # # image.save('blackandwhite.png')
-# print pytesseract.image_to_string(image)
+print pytesseract.image_to_string(imfile)
 print('done')
