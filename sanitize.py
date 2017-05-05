5cf16174e0d4e2553184782ad29bae7f7918469b
Scott O'Malley <somalley@guidewire.com>
2017-05-05 14:21:34 +0100

    Sanitize now checks all relevant fields

--- sanitize.py
+++ sanitize.py
@@ -30,46 +30,45 @@ with open('./assets/metadata/seals.json') as data_file:
 with open('./rawherodata.json') as hero_json:
     heroes = json.load(hero_json)
     for hero in heroes:
-        # if hero["name"] not in names:
-        #     match = findNearestMatch(hero["name"], names)
-        #     print hero["name"] + ' -> ' + match
-        #     hero["name"] = match
+        if hero["name"] not in names:
+            match = findNearestMatch(hero["name"], names)
+            print hero["name"] + ' -> ' + match
+            hero["name"] = match
 
-        # if hero["weapon"] != "_" and hero["weapon"] not in weapons:
-        #     match = findNearestMatch(hero["weapon"], weapons)
-        #     print hero["weapon"] + ' -> ' + match
-        #     hero["weapon"] = match
+        if hero["weapon"] != "_" and hero["weapon"] not in weapons:
+            match = findNearestMatch(hero["weapon"], weapons)
+            print hero["weapon"] + ' -> ' + match
+            hero["weapon"] = match
 
-        # if hero["assist"] != "_" and hero["assist"] not in assists:
-        #     match = findNearestMatch(hero["assist"], assists)
-        #     print hero["assist"] + ' -> ' + match
-        #     hero["assist"] = match
+        if hero["assist"] != "_" and hero["assist"] not in assists:
+            match = findNearestMatch(hero["assist"], assists)
+            print hero["assist"] + ' -> ' + match
+            hero["assist"] = match
 
-        # if hero["special"] != "_" and hero["special"] not in specials:
-        #     match = findNearestMatch(hero["special"], specials)
-        #     print hero["special"] + ' -> ' + match
-        #     hero["special"] = match
+        if hero["special"] != "_" and hero["special"] not in specials:
+            match = findNearestMatch(hero["special"], specials)
+            print hero["special"] + ' -> ' + match
+            hero["special"] = match
 
-        # if hero["aSlot"] != "_" and hero["aSlot"] not in passives:
-        #     match = findNearestMatch(hero["aSlot"], passives)
-        #     print hero["aSlot"] + ' -> ' + match
-        #     hero["aSlot"] = match
+        if hero["aSlot"] != "_" and hero["aSlot"] not in passives:
+            match = findNearestMatch(hero["aSlot"], passives)
+            print hero["aSlot"] + ' -> ' + match
+            hero["aSlot"] = match
 
-        # if hero["bSlot"] != "_" and hero["bSlot"] not in passives:
-        #     match = findNearestMatch(hero["bSlot"], passives)
-        #     print hero["bSlot"] + ' -> ' + match
-        #     hero["bSlot"] = match
+        if hero["bSlot"] != "_" and hero["bSlot"] not in passives:
+            match = findNearestMatch(hero["bSlot"], passives)
+            print hero["bSlot"] + ' -> ' + match
+            hero["bSlot"] = match
 
-        # if hero["cSlot"] != "_" and hero["cSlot"] not in passives:
-        #     match = findNearestMatch(hero["cSlot"], passives)
-        #     print hero["cSlot"] + ' -> ' + match
-        #     hero["cSlot"] = match
+        if hero["cSlot"] != "_" and hero["cSlot"] not in passives:
+            match = findNearestMatch(hero["cSlot"], passives)
+            print hero["cSlot"] + ' -> ' + match
+            hero["cSlot"] = match
 
-        # if hero["sSlot"] != "_" and hero["sSlot"] not in seals:
-        #     match = findNearestMatch(hero["sSlot"], seals)
-        #     print hero["sSlot"] + ' -> ' + match
-        #     hero["sSlot"] = match
-        print hero["exp"]
+        if hero["sSlot"] != "_" and hero["sSlot"] not in seals:
+            match = findNearestMatch(hero["sSlot"], seals)
+            print hero["sSlot"] + ' -> ' + match
+            hero["sSlot"] = match
 
     with open('sanatizedherodata.json', 'w') as outfile:
         json.dump(heroes, outfile)
